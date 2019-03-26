2dcfd1170247cb8656b22f40a2dc3e745045d61b
Syed Ganih <msprouse843@gmail.com>
2019-03-27 01:18:28 +0800

    Working for supplier

--- AdminSupplierForm.Designer.cs
+++ AdminSupplierForm.Designer.cs
@@ -28,84 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.contactnum = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.website = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.faxnum = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.telephonenum = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lblID = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.cmbSearchBy = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
-            this.supplieraddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnSearch = new System.Windows.Forms.Button();
             this.txtSearchBy = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.supplieraname = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.supplierid = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnPrintPreview = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.tblSupplier = new System.Windows.Forms.DataGridView();
-            this.lblUSERTYPE = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAddSupplier = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.tblSupplier)).BeginInit();
             this.SuspendLayout();
-            // 
-            // contactnum
-            // 
-            this.contactnum.FillWeight = 99.76229F;
-            this.contactnum.HeaderText = "CONTACT #";
-            this.contactnum.Name = "contactnum";
-            // 
-            // website
-            // 
-            this.website.FillWeight = 104.1478F;
-            this.website.HeaderText = "WEBSITE";
-            this.website.Name = "website";
-            // 
-            // email
-            // 
-            this.email.FillWeight = 103.2491F;
-            this.email.HeaderText = "E-MAIL ADD";
-            this.email.Name = "email";
-            // 
-            // faxnum
-            // 
-            this.faxnum.FillWeight = 102.2298F;
-            this.faxnum.HeaderText = "FAX #";
-            this.faxnum.Name = "faxnum";
-            // 
-            // telephonenum
-            // 
-            this.telephonenum.FillWeight = 101.0736F;
-            this.telephonenum.HeaderText = "TELEPHONE #";
-            this.telephonenum.Name = "telephonenum";
-            // 
-            // lblID
-            // 
-            this.lblID.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lblID.AutoSize = true;
-            this.lblID.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblID.Location = new System.Drawing.Point(40, 32);
-            this.lblID.Name = "lblID";
-            this.lblID.Size = new System.Drawing.Size(23, 19);
-            this.lblID.TabIndex = 71;
-            this.lblID.Text = "ID";
-            // 
-            // label8
-            // 
-            this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(13, 32);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(27, 19);
-            this.label8.TabIndex = 70;
-            this.label8.Text = "ID:";
             // 
             // label6
             // 
@@ -140,12 +79,6 @@
             this.label7.TabIndex = 58;
             this.label7.Text = "SEARCH BY:";
             // 
-            // supplieraddress
-            // 
-            this.supplieraddress.FillWeight = 98.27497F;
-            this.supplieraddress.HeaderText = "SUPPLIER ADDRESS";
-            this.supplieraddress.Name = "supplieraddress";
-            // 
             // btnSearch
             // 
             this.btnSearch.Anchor = System.Windows.Forms.AnchorStyles.None;
@@ -168,29 +101,6 @@
             this.txtSearchBy.Name = "txtSearchBy";
             this.txtSearchBy.Size = new System.Drawing.Size(329, 27);
             this.txtSearchBy.TabIndex = 60;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(11, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(80, 19);
-            this.label2.TabIndex = 56;
-            this.label2.Text = "WELCOME";
-            // 
-            // supplieraname
-            // 
-            this.supplieraname.FillWeight = 96.58797F;
-            this.supplieraname.HeaderText = "SUPPLIER NAME";
-            this.supplieraname.Name = "supplieraname";
-            // 
-            // supplierid
-            // 
-            this.supplierid.FillWeight = 94.67457F;
-            this.supplierid.HeaderText = "SUPPLIER ID";
-            this.supplierid.Name = "supplierid";
             // 
             // btnPrintPreview
             // 
@@ -239,50 +149,70 @@
             // 
             // tblSupplier
             // 
+            this.tblSupplier.AllowUserToAddRows = false;
+            this.tblSupplier.AllowUserToDeleteRows = false;
             this.tblSupplier.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.tblSupplier.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.tblSupplier.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.supplierid,
-            this.supplieraname,
-            this.supplieraddress,
-            this.contactnum,
-            this.telephonenum,
-            this.faxnum,
-            this.email,
-            this.website});
             this.tblSupplier.Location = new System.Drawing.Point(9, 8);
             this.tblSupplier.Name = "tblSupplier";
             this.tblSupplier.Size = new System.Drawing.Size(1215, 253);
             this.tblSupplier.TabIndex = 0;
             // 
-            // lblUSERTYPE
+            // btnDelete
             // 
-            this.lblUSERTYPE.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lblUSERTYPE.AutoSize = true;
-            this.lblUSERTYPE.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUSERTYPE.Location = new System.Drawing.Point(97, 9);
-            this.lblUSERTYPE.Name = "lblUSERTYPE";
-            this.lblUSERTYPE.Size = new System.Drawing.Size(75, 19);
-            this.lblUSERTYPE.TabIndex = 62;
-            this.lblUSERTYPE.Text = "USERTYPE";
+            this.btnDelete.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDelete.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(906, 422);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(186, 36);
+            this.btnDelete.TabIndex = 72;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEdit.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnEdit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEdit.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEdit.Location = new System.Drawing.Point(714, 422);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(186, 36);
+            this.btnEdit.TabIndex = 73;
+            this.btnEdit.Text = "EDIT";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            // 
+            // btnAddSupplier
+            // 
+            this.btnAddSupplier.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAddSupplier.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.btnAddSupplier.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddSupplier.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddSupplier.Location = new System.Drawing.Point(522, 422);
+            this.btnAddSupplier.Name = "btnAddSupplier";
+            this.btnAddSupplier.Size = new System.Drawing.Size(186, 36);
+            this.btnAddSupplier.TabIndex = 74;
+            this.btnAddSupplier.Text = "ADD SUPPLIER";
+            this.btnAddSupplier.UseVisualStyleBackColor = false;
             // 
             // AdminSupplierForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1294, 467);
-            this.Controls.Add(this.lblID);
-            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnAddSupplier);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.cmbSearchBy);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.btnSearch);
             this.Controls.Add(this.txtSearchBy);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.btnPrintPreview);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
-            this.Controls.Add(this.lblUSERTYPE);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminSupplierForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -298,27 +228,18 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridViewTextBoxColumn contactnum;
-        private System.Windows.Forms.DataGridViewTextBoxColumn website;
-        private System.Windows.Forms.DataGridViewTextBoxColumn email;
-        private System.Windows.Forms.DataGridViewTextBoxColumn faxnum;
-        private System.Windows.Forms.DataGridViewTextBoxColumn telephonenum;
-        private System.Windows.Forms.Label lblID;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.ComboBox cmbSearchBy;
         private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.DataGridViewTextBoxColumn supplieraddress;
         private System.Windows.Forms.Button btnSearch;
         private System.Windows.Forms.TextBox txtSearchBy;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn supplieraname;
-        private System.Windows.Forms.DataGridViewTextBoxColumn supplierid;
         private System.Windows.Forms.Button btnPrintPreview;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.DataGridView tblSupplier;
-        private System.Windows.Forms.Label lblUSERTYPE;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAddSupplier;
     }
 }
--- StockClerkInventoryMenu.Designer.cs
+++ StockClerkInventoryMenu.Designer.cs
@@ -28,11 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.msStockIn = new System.Windows.Forms.ToolStripMenuItem();
+            this.msSales = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlContainer = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.msStockReturn = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlContainer.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msStockIn
+            // 
+            this.msStockIn.Font = new System.Drawing.Font("Calibri", 14F, System.Drawing.FontStyle.Bold);
+            this.msStockIn.Name = "msStockIn";
+            this.msStockIn.Padding = new System.Windows.Forms.Padding(250, 0, 4, 0);
+            this.msStockIn.Size = new System.Drawing.Size(340, 27);
+            this.msStockIn.Text = "STOCK IN";
+            // 
+            // msSales
+            // 
+            this.msSales.Name = "msSales";
+            this.msSales.Padding = new System.Windows.Forms.Padding(200, 0, 4, 0);
+            this.msSales.Size = new System.Drawing.Size(265, 27);
+            this.msSales.Text = "SALES";
+            // 
+            // pnlContainer
+            // 
+            this.pnlContainer.Controls.Add(this.menuStrip1);
+            this.pnlContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContainer.Location = new System.Drawing.Point(0, 0);
+            this.pnlContainer.Name = "pnlContainer";
+            this.pnlContainer.Size = new System.Drawing.Size(900, 455);
+            this.pnlContainer.TabIndex = 4;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.menuStrip1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.msStockIn,
+            this.msSales,
+            this.msStockReturn});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(900, 31);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // msStockReturn
+            // 
+            this.msStockReturn.Name = "msStockReturn";
+            this.msStockReturn.Padding = new System.Windows.Forms.Padding(150, 0, 4, 0);
+            this.msStockReturn.Size = new System.Drawing.Size(287, 27);
+            this.msStockReturn.Text = "STOCK RETURN";
+            // 
+            // StockClerkInventoryMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 455);
+            this.Controls.Add(this.pnlContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StockClerkInventoryMenu";
             this.Text = "StockClerkInventoryMenu";
+            this.pnlContainer.ResumeLayout(false);
+            this.pnlContainer.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem msStockIn;
+        private System.Windows.Forms.ToolStripMenuItem msSales;
+        private System.Windows.Forms.Panel pnlContainer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem msStockReturn;
     }
 }
